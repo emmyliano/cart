2f159ffe01f89f0465807f49b2e88704894888c7
Akemu Emmanuel <akemuemmy@gmail.com>
2024-04-01 05:33:20 -0700

    fixed bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:cart/onBoardingScreens/onboard_screen_1.dart';
 import 'package:cart/onBoardingScreens/onboard_screen_2.dart';
 import 'package:cart/onBoardingScreens/onboard_screen_3.dart';
+import 'package:cart/sign_in_screen.dart';
 import 'package:flutter/material.dart';
-
 
 void main() {
   runApp(const MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -16,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: OnBoardScreen3(),
+      home: SignInScreen(),
     );
   }
 }
